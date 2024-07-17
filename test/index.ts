d674c71c077b461e68011559a9bdfec33ecb302e
Rachel Fenichel <fenichel@google.com>
2024-07-17 16:06:46 -0700

    Revert "feat: add p5 blocks and execution (#26)"
    This reverts commit 31bdcb23698900910dbd921f551e17f8909952c7.

--- test/index.ts
+++ test/index.ts
@@ -5,24 +5,11 @@
  */
 
 import * as Blockly from 'blockly';
-import {installAllBlocks as installColourBlocks} from '@blockly/field-colour';
-import {createPlayground} from '@blockly/dev-tools';
+import {toolboxCategories, createPlayground} from '@blockly/dev-tools';
 import {KeyboardNavigation} from '../src/index';
-// @ts-expect-error No types in js file
-import {forBlock} from '../src/blocks/p5_generators';
-// @ts-expect-error No types in js file
-import {blocks} from '../src/blocks/p5_blocks';
-// @ts-expect-error No types in js file
-import {toolbox} from '../src/blocks/toolbox.js';
-
-import {javascriptGenerator} from 'blockly/javascript';
-// @ts-expect-error No types in js file
-import {save, load} from './loadTestBlocks';
-import {runCode, registerRunCodeShortcut} from './runCode';
 
 /**
- * Create the workspace, including installing keyboard navigation and
- * change listeners.
+ * Create a workspace.
  *
  * @param blocklyDiv The blockly container div.
  * @param options The Blockly options.
@@ -35,65 +22,32 @@ function createWorkspace(
   const workspace = Blockly.inject(blocklyDiv, options);
 
   const plugin = new KeyboardNavigation(workspace);
-  registerRunCodeShortcut();
-
-  // Disable blocks that aren't inside the setup or draw loops.
-  workspace.addChangeListener(Blockly.Events.disableOrphans);
-
-  // Load the initial state from storage and run the code.
-  load(workspace);
-  runCode();
 
   return workspace;
 }
 
-/**
- * Install p5.js blocks and generators.
- */
-function addP5() {
-  // Installs all four blocks, the colour field, and all language generators.
-  installColourBlocks({
-    javascript: javascriptGenerator,
-  });
-  Blockly.common.defineBlocks(blocks);
-  Object.assign(javascriptGenerator.forBlock, forBlock);
-  javascriptGenerator.addReservedWords('sketch');
-}
-
-/**
- * Clean up the appearance of the playground by hiding the developer
- * drawer.
- */
 function setOptionsDefaultCollapsed() {
   const key = 'playgroundState_@blockly/keyboard-experiment';
-  const state = localStorage.getItem(key);
+  let state = localStorage.getItem(key);
   if (state === null) {
-    localStorage.setItem(
-      key,
-      JSON.stringify({
-        activeTab: 'JSON',
-        // This is the thing we're actually changing.
-        playgroundOpen: false,
-        autoGenerate: true,
-        workspaceJson: '',
-      }),
-    );
+    localStorage.setItem(key, JSON.stringify({
+      activeTab: 'JSON',
+      // This is the thing we're actually changing.
+      playgroundOpen: false,
+      autoGenerate: true,
+      workspaceJson: '',
+    }));
   }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  addP5();
   setOptionsDefaultCollapsed();
   const defaultOptions = {
-    toolbox: toolbox,
-    renderer: 'zelos',
+    toolbox: toolboxCategories,
   };
-
   createPlayground(
     document.getElementById('root')!,
     createWorkspace,
     defaultOptions,
   );
-
-  document.getElementById('run')?.addEventListener('click', runCode);
 });
