e4f239a41c9a9650a9a6c939781fb5edb89d41fd
Rachel Fenichel <fenichel@google.com>
2024-07-18 11:17:08 -0700

    feat: stop using the dev-tools drawer

--- test/index.ts
+++ test/index.ts
@@ -6,7 +6,6 @@
 
 import * as Blockly from 'blockly';
 import {installAllBlocks as installColourBlocks} from '@blockly/field-colour';
-import {createPlayground} from '@blockly/dev-tools';
 import {KeyboardNavigation} from '../src/index';
 // @ts-expect-error No types in js file
 import {forBlock} from '../src/blocks/p5_generators';
@@ -24,14 +23,14 @@ import {runCode, registerRunCodeShortcut} from './runCode';
  * Create the workspace, including installing keyboard navigation and
  * change listeners.
  *
- * @param blocklyDiv The blockly container div.
- * @param options The Blockly options.
  * @returns The created workspace.
  */
-function createWorkspace(
-  blocklyDiv: HTMLElement,
-  options: Blockly.BlocklyOptions,
-): Blockly.WorkspaceSvg {
+function createWorkspace(): Blockly.WorkspaceSvg {
+  const options = {
+    toolbox: toolbox,
+    renderer: 'zelos',
+  };
+  const blocklyDiv = document.getElementById('blocklyDiv')!;
   const workspace = Blockly.inject(blocklyDiv, options);
 
   const plugin = new KeyboardNavigation(workspace);
@@ -60,40 +59,8 @@ function addP5() {
   javascriptGenerator.addReservedWords('sketch');
 }
 
-/**
- * Clean up the appearance of the playground by hiding the developer
- * drawer.
- */
-function setOptionsDefaultCollapsed() {
-  const key = 'playgroundState_@blockly/keyboard-experiment';
-  const state = localStorage.getItem(key);
-  if (state === null) {
-    localStorage.setItem(
-      key,
-      JSON.stringify({
-        activeTab: 'JSON',
-        // This is the thing we're actually changing.
-        playgroundOpen: false,
-        autoGenerate: true,
-        workspaceJson: '',
-      }),
-    );
-  }
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   addP5();
-  setOptionsDefaultCollapsed();
-  const defaultOptions = {
-    toolbox: toolbox,
-    renderer: 'zelos',
-  };
-
-  createPlayground(
-    document.getElementById('root')!,
-    createWorkspace,
-    defaultOptions,
-  );
-
+  createWorkspace();
   document.getElementById('run')?.addEventListener('click', runCode);
 });
