16321874b25fdb5d2a025141a44d2dc9dce4bda0
Rachel Fenichel <fenichel@google.com>
2024-09-23 07:57:17 -0700

    feat: testing scenario (#58)
    * feat: simplify toolbox blocks for testing discussion
    
    * feat: add simpleCircle scenario
    
    * chore: format
    
    * chore: remove commented out code

--- src/blocks/toolbox.js
+++ src/blocks/toolbox.js
@@ -20,28 +20,6 @@ export const toolbox = {
     },
     {
       kind: 'block',
-      type: 'p5_stroke',
-      inputs: {
-        COLOR: {
-          shadow: {
-            type: 'colour_picker',
-          },
-        },
-      },
-    },
-    {
-      kind: 'block',
-      type: 'p5_fill',
-      inputs: {
-        COLOR: {
-          shadow: {
-            type: 'colour_picker',
-          },
-        },
-      },
-    },
-    {
-      kind: 'block',
       type: 'colour_random',
     },
     {
@@ -55,106 +33,6 @@ export const toolbox = {
         COLOR: {
           shadow: {
             type: 'colour_picker',
-          },
-        },
-      },
-    },
-    {
-      kind: 'block',
-      type: 'p5_ellipse',
-      inline: 'true',
-      inputs: {
-        X: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 100,
-            },
-          },
-        },
-        Y: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 100,
-            },
-          },
-        },
-        WIDTH: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 50,
-            },
-          },
-        },
-        HEIGHT: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 50,
-            },
-          },
-        },
-      },
-    },
-    {
-      kind: 'block',
-      type: 'math_random_int',
-      inputs: {
-        FROM: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 0,
-            },
-          },
-        },
-        TO: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 400,
-            },
-          },
-        },
-      },
-    },
-    {
-      kind: 'block',
-      type: 'controls_repeat_ext',
-      inputs: {
-        TIMES: {
-          block: {
-            type: 'math_number',
-            fields: {
-              NUM: 5,
-            },
-          },
-        },
-      },
-    },
-    {
-      kind: 'block',
-      type: 'math_arithmetic',
-      fields: {
-        OP: 'ADD',
-      },
-      inputs: {
-        A: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 1,
-            },
-          },
-        },
-        B: {
-          shadow: {
-            type: 'math_number',
-            fields: {
-              NUM: 1,
-            },
           },
         },
       },
