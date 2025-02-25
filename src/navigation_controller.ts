258aa41b6da45209e4697fca827b103fc5f418d5
Christopher Allen <cpcallen+git@google.com>
2025-02-25 19:11:42 +0000

    fix: Capitalise insert menu option
    Use title case for consistency with existing Blockly context
    menu items.

--- src/navigation_controller.ts
+++ src/navigation_controller.ts
@@ -728,7 +728,7 @@ export class NavigationController {
   protected registerInsertAction() {
     const insertAboveAction: ContextMenuRegistry.RegistryItem = {
       displayText: (scope: Scope) =>
-        scope.block?.previousConnection ? 'Insert block above' : 'Insert block',
+        scope.block?.previousConnection ? 'Insert Block Above' : 'Insert Block',
       preconditionFn: (scope: Scope) => {
         const block = scope.block ?? scope.connection?.getSourceBlock();
         const ws = block?.workspace as WorkspaceSvg | null;
