1870e5e536f6124012a3a33278173e3317d32cbc
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-03-25 15:34:23 +0000

    chore: remove reference to fixed bug

--- src/navigation.ts
+++ src/navigation.ts
@@ -455,12 +455,10 @@ export class Navigation {
   ) {
     if (relatedTarget === null) {
       // Workaround:
-      // Skip document.body/null case until these blur bugs are fixed to avoid
+      // Skip document.body/null case until this blur bugs is fixed to avoid
       // flipping to passive focus as the user moves their mouse over the
-      // dropdown/widget at the cost of clicks on body showing the wrong focus
-      // style.
+      // colour picker.
       // https://github.com/google/blockly-samples/issues/2498
-      // https://github.com/google/blockly/issues/8819
       return;
     }
     if (relatedTarget !== getWorkspaceElement(workspace)) {
