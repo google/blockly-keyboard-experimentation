1c6f2a584bf57562e5846918195a5abd52a832e3
Rachel Fenichel <fenichel@google.com>
2025-04-02 17:27:52 -0700

    fix: position of block at the start of a drag

--- src/actions/clipboard.ts
+++ src/actions/clipboard.ts
@@ -5,7 +5,6 @@
  */
 
 import {
-  ASTNode,
   ContextMenuRegistry,
   Gesture,
   ShortcutRegistry,
--- src/keyboard_drag_strategy.ts
+++ src/keyboard_drag_strategy.ts
@@ -6,4 +6,12 @@
 
 import {dragging} from 'blockly';
 
-export class KeyboardDragStrategy extends dragging.BlockDragStrategy {}
+export class KeyboardDragStrategy extends dragging.BlockDragStrategy {
+  override startDrag(e?: PointerEvent) {
+    super.startDrag(e);
+    // Set position of the dragging block, so that it doesn't pop
+    // to the top left of the workspace.
+    // @ts-expect-error block and startLoc are private.
+    this.block.moveDuringDrag(this.startLoc);
+  }
+}
