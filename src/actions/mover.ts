6b98bde62ed999d221f42865966252202f952416
Rachel Fenichel <fenichel@google.com>
2025-04-08 09:40:46 -0700

    feat: show drag preview immediately on start (#392)

--- src/actions/mover.ts
+++ src/actions/mover.ts
@@ -115,6 +115,7 @@ export class Mover {
     this.moves.set(workspace, info);
     // Begin drag.
     dragger.onDragStart(info.fakePointerEvent('pointerdown'));
+    info.dragger.onDrag(info.fakePointerEvent('pointermove'), info.totalDelta);
     return true;
   }
 
