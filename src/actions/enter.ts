6a2046fb5d06d147c07f8c0cc6b77d6740b44135
Aaron Dodson <adodson@google.com>
2025-05-19 11:27:04 -0700

    feat: Support activating focused icons.

--- src/actions/enter.ts
+++ src/actions/enter.ts
@@ -14,6 +14,7 @@ import {
   RenderedConnection,
   WorkspaceSvg,
   Field,
+  icons,
   FocusableTreeTraverser,
 } from 'blockly/core';
 
@@ -106,6 +107,8 @@ export class EnterAction {
       curNode instanceof WorkspaceSvg
     ) {
       this.navigation.openToolboxOrFlyout(workspace);
+    } else if (curNode instanceof icons.Icon) {
+      curNode.onClick();
     }
   }
 
