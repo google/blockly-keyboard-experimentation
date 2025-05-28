21895c70f04ab3d4b82cf19156e54bcc331e40c4
Erik Pasternak <epastern@google.com>
2025-05-27 17:13:13 -0700

    Add tests for real

---
+++ test/webdriverio/test/actions_test.ts
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as chai from 'chai';
+import {Key} from 'webdriverio';
+import {
+  checkActionPrecondition,
+  getFocusedBlockType,
+  moveToToolboxCategory,
+  PAUSE_TIME,
+  setCurrentCursorNodeById,
+  tabNavigateToWorkspace,
+  testFileLocations,
+  testSetup,
+} from './test_setup.js';
+
+suite('Menus test', function () {
+  // Setting timeout to unlimited as these tests take longer time to run
+  this.timeout(0);
+
+  // Clear the workspace and load start blocks
+  setup(async function () {
+    this.browser = await testSetup(testFileLocations.BASE);
+    await this.browser.pause(PAUSE_TIME);
+  });
+
+  test('Menu action returns true normally', async function () {
+    // Navigate to draw_circle_1.
+    await tabNavigateToWorkspace(this.browser);
+    await setCurrentCursorNodeById(this.browser, 'draw_circle_1');
+    chai.assert.isTrue(
+      await checkActionPrecondition(this.browser, 'menu'),
+      'The menu should be openable on a block',
+    );
+  });
+
+  test('Menu action returns true in the toolbox', async function () {
+    // Navigate to draw_circle_1.
+    await tabNavigateToWorkspace(this.browser);
+    await setCurrentCursorNodeById(this.browser, 'draw_circle_1');
+    // Navigate to a toolbox category
+    await moveToToolboxCategory(this.browser, 'Functions');
+    // Move to flyout.
+    await this.browser.keys(Key.ArrowRight);
+
+    chai.assert.isTrue(
+      await checkActionPrecondition(this.browser, 'menu'),
+      'The menu should be openable on a block in the toolbox',
+    );
+  });
+
+  test('Menu action returns false during drag', async function () {
+    // Navigate to draw_circle_1.
+    await tabNavigateToWorkspace(this.browser);
+    await setCurrentCursorNodeById(this.browser, 'draw_circle_1');
+    // Start moving the block
+    await this.browser.keys('m');
+    chai.assert.isFalse(
+      await checkActionPrecondition(this.browser, 'menu'),
+      'The menu should not be openable during a move',
+    );
+  });
+});
