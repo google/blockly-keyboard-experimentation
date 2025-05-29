543613786d8466e2361b8dbd70baff0b3800b6d1
Erik Pasternak <epastern@google.com>
2025-05-28 20:51:40 -0700

    Move buttons block to misc category

--- test/blocks/toolbox.js
+++ test/blocks/toolbox.js
@@ -54,10 +54,6 @@ export const p5CategoryContents = [
     kind: 'block',
     type: 'write_text_without_shadow',
   },
-  {
-    kind: 'block',
-    type: 'buttons_block'
-  },
 ];
 
 export const toolbox = {
