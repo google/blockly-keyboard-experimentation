8ac253c27f5254fcd5da382bfabe160902141b75
Aaron Dodson <adodson@google.com>
2025-07-15 11:51:35 -0700

    chore: Satisfy the linter.

--- src/navigation_deferring_toolbox.ts
+++ src/navigation_deferring_toolbox.ts
@@ -6,13 +6,20 @@
 
 import * as Blockly from 'blockly/core';
 
+/**
+ * Toolbox class that does not handle keyboard navigation.
+ */
 export class NavigationDeferringToolbox extends Blockly.Toolbox {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   protected override onKeyDown_(e: KeyboardEvent) {
     // No-op, prevent keyboard handling by superclass in order to defer to
     // global keyboard navigation.
   }
 }
 
+/**
+ * Registers the navigation-deferring toolbox with Blockly.
+ */
 export function registerNavigationDeferringToolbox() {
   Blockly.registry.register(
     Blockly.registry.Type.TOOLBOX,
