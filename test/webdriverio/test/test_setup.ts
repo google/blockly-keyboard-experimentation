3e04bdfb0b406301231483cd5d064b7c4bfd6a9b
Christopher Allen <cpcallen+git@google.com>
2025-08-28 12:14:16 +0100

    fix(tests): Suppress tsc unintentional comparison error
    When PAUSE_TIME is set to a value other than 0 the comparison
    in sendKeyAndWait generates a TS unintentionl comparison error.

--- test/webdriverio/test/test_setup.ts
+++ test/webdriverio/test/test_setup.ts
@@ -558,6 +558,7 @@ export async function sendKeyAndWait(
   keys: string | string[],
   times = 1,
 ) {
+  // @ts-ignore: Unintentional comparison error
   if (PAUSE_TIME === 0) {
     // Send all keys in one call if no pauses needed.
     keys = Array(times).fill(keys).flat();
